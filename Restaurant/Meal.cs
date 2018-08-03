a16ac428a9eaef1a38f828ea198f232f217f1b6d
Przemyslaw Springer <przmk01@gmail.com>
2018-08-03 13:26:45 +0200

    Collection of mandatory additives added to Meal, new factory for mandatory additives

--- Restaurant/Meal.cs
+++ Restaurant/Meal.cs
@@ -12,15 +12,34 @@ namespace Restaurant
         public decimal Cost { get; }
         public string Currency { get; }
         public MealType Type { get; }
-        public IEnumerable<Additive> PossibeAdditives { get; }
+        public IEnumerable<Additive> AvailableAdditives { get; }
+        public IEnumerable<Additive> AvailableMandatoryAdditives { get; }
         public IAdditiveCollection SelectedAdditives { get; }
 
 
-        public Meal(string name, decimal cost, MealType type, IAdditiveFactory possibleAdditivesFactory, IAdditiveCollection additiveCollection)
-            : this(name, cost, "zł", type, possibleAdditivesFactory, additiveCollection) { }
+        /// <summary>
+        /// Creates a Meal object. Sets Currency field as default value: "zł".
+        /// </summary>
+        /// <param name="name">Name of a meal</param>
+        /// <param name="cost">Cost of a meal</param>
+        /// <param name="type">Type of a meal (pizza, soup, etc.)</param>
+        /// <param name="availableAdditivesFactory">Object which creates a collection of available additives for the meal</param>
+        /// <param name="availableMandatoryAdditivesFactory">Object which creates a collection of available additives for the meal from which one must be added to the meal (rice, potatoes, etc.)</param>
+        /// <param name="additiveCollection">A collection to which selected additives are added</param>
+        public Meal(string name, decimal cost, MealType type, IAdditiveFactory availableAdditivesFactory, IAdditiveFactory availableMandatoryAdditivesFactory, IAdditiveCollection additiveCollection)
+            : this(name, cost, "zł", type, availableAdditivesFactory, availableMandatoryAdditivesFactory, additiveCollection) { }
 
 
-        public Meal(string name, decimal cost, string currency, MealType type, IAdditiveFactory possibleAdditivesFactory, IAdditiveCollection additiveCollection)
+        /// <summary>
+        /// Creates a Meal object.
+        /// </summary>
+        /// <param name="name">Name of a meal</param>
+        /// <param name="cost">Cost of a meal</param>
+        /// <param name="type">Type of a meal (pizza, soup, etc.)</param>
+        /// <param name="availableAdditivesFactory">Object which creates a collection of available additives for the meal</param>
+        /// <param name="availableMandatoryAdditivesFactory">Object which creates a collection of available additives for the meal from which one must be added to the meal (rice, potatoes, etc.)</param>
+        /// <param name="additiveCollection">A collection to which selected additives are added</param>
+        public Meal(string name, decimal cost, string currency, MealType type, IAdditiveFactory availableAdditivesFactory, IAdditiveFactory availableMandatoryAdditivesFactory, IAdditiveCollection additiveCollection)
         {
             if (String.IsNullOrWhiteSpace(name))
                 throw new ArgumentNullException("name", "Meal constructor: name cannot be empty or null.");
@@ -31,15 +50,19 @@ namespace Restaurant
             if (String.IsNullOrWhiteSpace(currency))
                 throw new ArgumentNullException("currency", "Meal constructor: currency cannot be empty or null.");
 
-            if (possibleAdditivesFactory == null)
-                throw new ArgumentNullException("possibleAdditivesFactory", "Meal constructor.");
+            if (availableAdditivesFactory == null)
+                throw new ArgumentNullException("availableAdditivesFactory", "Meal constructor.");
+
+            if (availableMandatoryAdditivesFactory == null)
+                throw new ArgumentNullException("availableMandatoryAdditives", "Meal constructor.");
 
             this.Type = type ?? throw new ArgumentNullException("type", "Meal constructor: meal type cannot be null.");
             this.SelectedAdditives = additiveCollection ?? throw new ArgumentNullException("additiveCollection", "Meal constructor");
             this.Name = name;
             this.Cost = cost;
             this.Currency = currency;
-            this.PossibeAdditives = possibleAdditivesFactory.CreateAdditives();
+            this.AvailableAdditives = availableAdditivesFactory.CreateAdditives();
+            this.AvailableMandatoryAdditives = availableMandatoryAdditivesFactory.CreateAdditives();
         }
     }
 }
---
+++ Restaurant/MeatAdditiveFactory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant
+{
+    public class MeatAdditiveFactory : IAdditiveFactory
+    {
+        public IEnumerable<Additive> CreateAdditives()
+        {
+            decimal cost = 0m;
+
+            return new List<Additive>
+            {
+                new Additive("Frytki", cost),
+                new Additive("Ryż", cost),
+                new Additive("Ziemniaki", cost)
+            };
+        }
+    }
+}
