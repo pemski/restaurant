09d67c7f5edf2825a6c5d72b850f0e315ccdc198
Przemyslaw Springer <przmk01@gmail.com>
2018-08-07 19:45:32 +0200

    Disabling minimize/maximize box on mail form

--- Restaurant/FMail.Designer.cs
+++ Restaurant/FMail.Designer.cs
@@ -29,24 +29,24 @@
         private void InitializeComponent()
         {
             this.pMain = new System.Windows.Forms.Panel();
-            this.eSender = new System.Windows.Forms.TextBox();
-            this.lSender = new System.Windows.Forms.Label();
+            this.bSend = new System.Windows.Forms.Button();
+            this.gbSSL = new System.Windows.Forms.GroupBox();
             this.rbSSLOn = new System.Windows.Forms.RadioButton();
             this.rbSSLOff = new System.Windows.Forms.RadioButton();
-            this.lPassword = new System.Windows.Forms.Label();
-            this.ePassword = new System.Windows.Forms.TextBox();
-            this.lReceiver = new System.Windows.Forms.Label();
-            this.eReceiver = new System.Windows.Forms.TextBox();
+            this.lSsl = new System.Windows.Forms.Label();
+            this.lPort = new System.Windows.Forms.Label();
+            this.ePort = new System.Windows.Forms.NumericUpDown();
             this.lSmtp = new System.Windows.Forms.Label();
             this.eSmtp = new System.Windows.Forms.TextBox();
-            this.ePort = new System.Windows.Forms.NumericUpDown();
-            this.lPort = new System.Windows.Forms.Label();
-            this.lSsl = new System.Windows.Forms.Label();
-            this.gbSSL = new System.Windows.Forms.GroupBox();
-            this.bSend = new System.Windows.Forms.Button();
+            this.lReceiver = new System.Windows.Forms.Label();
+            this.eReceiver = new System.Windows.Forms.TextBox();
+            this.lPassword = new System.Windows.Forms.Label();
+            this.ePassword = new System.Windows.Forms.TextBox();
+            this.lSender = new System.Windows.Forms.Label();
+            this.eSender = new System.Windows.Forms.TextBox();
             this.pMain.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.ePort)).BeginInit();
             this.gbSSL.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ePort)).BeginInit();
             this.SuspendLayout();
             // 
             // pMain
@@ -71,21 +71,26 @@
             this.pMain.Size = new System.Drawing.Size(251, 248);
             this.pMain.TabIndex = 0;
             // 
-            // eSender
+            // bSend
             // 
-            this.eSender.Location = new System.Drawing.Point(71, 12);
-            this.eSender.Name = "eSender";
-            this.eSender.Size = new System.Drawing.Size(168, 20);
-            this.eSender.TabIndex = 0;
+            this.bSend.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.bSend.Location = new System.Drawing.Point(143, 206);
+            this.bSend.Name = "bSend";
+            this.bSend.Size = new System.Drawing.Size(96, 30);
+            this.bSend.TabIndex = 6;
+            this.bSend.Text = "Wyślij";
+            this.bSend.UseVisualStyleBackColor = true;
+            this.bSend.Click += new System.EventHandler(this.bSend_Click);
             // 
-            // lSender
+            // gbSSL
             // 
-            this.lSender.AutoSize = true;
-            this.lSender.Location = new System.Drawing.Point(12, 15);
-            this.lSender.Name = "lSender";
-            this.lSender.Size = new System.Drawing.Size(53, 13);
-            this.lSender.TabIndex = 1;
-            this.lSender.Text = "Nadawca";
+            this.gbSSL.Controls.Add(this.rbSSLOn);
+            this.gbSSL.Controls.Add(this.rbSSLOff);
+            this.gbSSL.Location = new System.Drawing.Point(71, 155);
+            this.gbSSL.Name = "gbSSL";
+            this.gbSSL.Size = new System.Drawing.Size(119, 34);
+            this.gbSSL.TabIndex = 5;
+            this.gbSSL.TabStop = false;
             // 
             // rbSSLOn
             // 
@@ -110,54 +115,23 @@
             this.rbSSLOff.Text = "Nie";
             this.rbSSLOff.UseVisualStyleBackColor = true;
             // 
-            // lPassword
+            // lSsl
             // 
-            this.lPassword.AutoSize = true;
-            this.lPassword.Location = new System.Drawing.Point(12, 41);
-            this.lPassword.Name = "lPassword";
-            this.lPassword.Size = new System.Drawing.Size(36, 13);
-            this.lPassword.TabIndex = 5;
-            this.lPassword.Text = "Hasło";
+            this.lSsl.AutoSize = true;
+            this.lSsl.Location = new System.Drawing.Point(12, 167);
+            this.lSsl.Name = "lSsl";
+            this.lSsl.Size = new System.Drawing.Size(27, 13);
+            this.lSsl.TabIndex = 12;
+            this.lSsl.Text = "SSL";
             // 
-            // ePassword
+            // lPort
             // 
-            this.ePassword.Location = new System.Drawing.Point(71, 38);
-            this.ePassword.Name = "ePassword";
-            this.ePassword.PasswordChar = '*';
-            this.ePassword.Size = new System.Drawing.Size(168, 20);
-            this.ePassword.TabIndex = 1;
-            // 
-            // lReceiver
-            // 
-            this.lReceiver.AutoSize = true;
-            this.lReceiver.Location = new System.Drawing.Point(12, 67);
-            this.lReceiver.Name = "lReceiver";
-            this.lReceiver.Size = new System.Drawing.Size(43, 13);
-            this.lReceiver.TabIndex = 7;
-            this.lReceiver.Text = "Adresat";
-            // 
-            // eReceiver
-            // 
-            this.eReceiver.Location = new System.Drawing.Point(71, 64);
-            this.eReceiver.Name = "eReceiver";
-            this.eReceiver.Size = new System.Drawing.Size(168, 20);
-            this.eReceiver.TabIndex = 2;
-            // 
-            // lSmtp
-            // 
-            this.lSmtp.AutoSize = true;
-            this.lSmtp.Location = new System.Drawing.Point(12, 105);
-            this.lSmtp.Name = "lSmtp";
-            this.lSmtp.Size = new System.Drawing.Size(37, 13);
-            this.lSmtp.TabIndex = 9;
-            this.lSmtp.Text = "SMTP";
-            // 
-            // eSmtp
-            // 
-            this.eSmtp.Location = new System.Drawing.Point(71, 102);
-            this.eSmtp.Name = "eSmtp";
-            this.eSmtp.Size = new System.Drawing.Size(168, 20);
-            this.eSmtp.TabIndex = 3;
+            this.lPort.AutoSize = true;
+            this.lPort.Location = new System.Drawing.Point(12, 131);
+            this.lPort.Name = "lPort";
+            this.lPort.Size = new System.Drawing.Size(26, 13);
+            this.lPort.TabIndex = 11;
+            this.lPort.Text = "Port";
             // 
             // ePort
             // 
@@ -181,44 +155,70 @@
             0,
             0});
             // 
-            // lPort
+            // lSmtp
             // 
-            this.lPort.AutoSize = true;
-            this.lPort.Location = new System.Drawing.Point(12, 131);
-            this.lPort.Name = "lPort";
-            this.lPort.Size = new System.Drawing.Size(26, 13);
-            this.lPort.TabIndex = 11;
-            this.lPort.Text = "Port";
+            this.lSmtp.AutoSize = true;
+            this.lSmtp.Location = new System.Drawing.Point(12, 105);
+            this.lSmtp.Name = "lSmtp";
+            this.lSmtp.Size = new System.Drawing.Size(37, 13);
+            this.lSmtp.TabIndex = 9;
+            this.lSmtp.Text = "SMTP";
             // 
-            // lSsl
+            // eSmtp
             // 
-            this.lSsl.AutoSize = true;
-            this.lSsl.Location = new System.Drawing.Point(12, 167);
-            this.lSsl.Name = "lSsl";
-            this.lSsl.Size = new System.Drawing.Size(27, 13);
-            this.lSsl.TabIndex = 12;
-            this.lSsl.Text = "SSL";
+            this.eSmtp.Location = new System.Drawing.Point(71, 102);
+            this.eSmtp.Name = "eSmtp";
+            this.eSmtp.Size = new System.Drawing.Size(168, 20);
+            this.eSmtp.TabIndex = 3;
             // 
-            // gbSSL
+            // lReceiver
             // 
-            this.gbSSL.Controls.Add(this.rbSSLOn);
-            this.gbSSL.Controls.Add(this.rbSSLOff);
-            this.gbSSL.Location = new System.Drawing.Point(71, 155);
-            this.gbSSL.Name = "gbSSL";
-            this.gbSSL.Size = new System.Drawing.Size(119, 34);
-            this.gbSSL.TabIndex = 5;
-            this.gbSSL.TabStop = false;
+            this.lReceiver.AutoSize = true;
+            this.lReceiver.Location = new System.Drawing.Point(12, 67);
+            this.lReceiver.Name = "lReceiver";
+            this.lReceiver.Size = new System.Drawing.Size(43, 13);
+            this.lReceiver.TabIndex = 7;
+            this.lReceiver.Text = "Adresat";
             // 
-            // bSend
+            // eReceiver
             // 
-            this.bSend.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.bSend.Location = new System.Drawing.Point(143, 206);
-            this.bSend.Name = "bSend";
-            this.bSend.Size = new System.Drawing.Size(96, 30);
-            this.bSend.TabIndex = 6;
-            this.bSend.Text = "Wyślij";
-            this.bSend.UseVisualStyleBackColor = true;
-            this.bSend.Click += new System.EventHandler(this.bSend_Click);
+            this.eReceiver.Location = new System.Drawing.Point(71, 64);
+            this.eReceiver.Name = "eReceiver";
+            this.eReceiver.Size = new System.Drawing.Size(168, 20);
+            this.eReceiver.TabIndex = 2;
+            // 
+            // lPassword
+            // 
+            this.lPassword.AutoSize = true;
+            this.lPassword.Location = new System.Drawing.Point(12, 41);
+            this.lPassword.Name = "lPassword";
+            this.lPassword.Size = new System.Drawing.Size(36, 13);
+            this.lPassword.TabIndex = 5;
+            this.lPassword.Text = "Hasło";
+            // 
+            // ePassword
+            // 
+            this.ePassword.Location = new System.Drawing.Point(71, 38);
+            this.ePassword.Name = "ePassword";
+            this.ePassword.PasswordChar = '*';
+            this.ePassword.Size = new System.Drawing.Size(168, 20);
+            this.ePassword.TabIndex = 1;
+            // 
+            // lSender
+            // 
+            this.lSender.AutoSize = true;
+            this.lSender.Location = new System.Drawing.Point(12, 15);
+            this.lSender.Name = "lSender";
+            this.lSender.Size = new System.Drawing.Size(53, 13);
+            this.lSender.TabIndex = 1;
+            this.lSender.Text = "Nadawca";
+            // 
+            // eSender
+            // 
+            this.eSender.Location = new System.Drawing.Point(71, 12);
+            this.eSender.Name = "eSender";
+            this.eSender.Size = new System.Drawing.Size(168, 20);
+            this.eSender.TabIndex = 0;
             // 
             // FMail
             // 
@@ -227,14 +227,16 @@
             this.ClientSize = new System.Drawing.Size(251, 248);
             this.Controls.Add(this.pMain);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "FMail";
             this.ShowIcon = false;
             this.Text = "Wysyłka zamówienia";
             this.pMain.ResumeLayout(false);
             this.pMain.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.ePort)).EndInit();
             this.gbSSL.ResumeLayout(false);
             this.gbSSL.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ePort)).EndInit();
             this.ResumeLayout(false);
 
         }
