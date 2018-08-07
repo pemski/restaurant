ae32d4fdd48dc3cfa018f798c64574731023be82
Przemyslaw Springer <przmk01@gmail.com>
2018-08-07 19:33:54 +0200

    Client commentary

--- Restaurant/Client.cs
+++ Restaurant/Client.cs
@@ -6,6 +6,10 @@ using System.Threading.Tasks;
 
 namespace Restaurant
 {
+    /// <summary>
+    /// Client only holding Order could be removed and just Order could be used, however it stays
+    /// in case any personal data (name, address) appears in the future.
+    /// </summary>
     public class Client
     {
         public Order Order { get; }
