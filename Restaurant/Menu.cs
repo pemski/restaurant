fe9545793534fdb5727a3e8790b2073b7e2c70cc
Przemyslaw Springer <przmk01@gmail.com>
2018-08-15 19:44:37 +0200

    Clearing Menu constructor

--- Restaurant/Menu.cs
+++ Restaurant/Menu.cs
@@ -13,25 +13,6 @@ namespace Restaurant
 
         public Menu()
         {
-            List<Meal> meals = new List<Meal>
-            {
-                new Meal("Margheritta", 20m, MealType.Pizza, new PizzaAdditiveFactory(), new NullAdditiveFactory(), new AdditiveHashSet()),
-                new Meal("Vegetariana", 22m, MealType.Pizza, new PizzaAdditiveFactory(), new NullAdditiveFactory(), new AdditiveHashSet()),
-                new Meal("Tosca", 25m, MealType.Pizza, new PizzaAdditiveFactory(), new NullAdditiveFactory(), new AdditiveHashSet()),
-                new Meal("Venecia", 25m, MealType.Pizza, new PizzaAdditiveFactory(), new NullAdditiveFactory(), new AdditiveHashSet()),
-
-                new Meal("Schabowy z frytkami/ryżem/ziemniakami", 30m, MealType.MainCourse, new MainCourseAdditiveFactory(), new MeatAdditiveFactory(), new AdditiveHashSet()),
-                new Meal("Ryba z frytkami", 28m, MealType.MainCourse, new MainCourseAdditiveFactory(), new NullAdditiveFactory(), new AdditiveHashSet()),
-                new Meal("Placek po węgiersku", 27m, MealType.MainCourse, new MainCourseAdditiveFactory(), new NullAdditiveFactory(), new AdditiveHashSet()),
-
-                new Meal("Pomidorowa", 12m, MealType.Soup, new NullAdditiveFactory(), new NullAdditiveFactory(), new AdditiveHashSet()),
-                new Meal("Rosół", 10m, MealType.Soup, new NullAdditiveFactory(), new NullAdditiveFactory(), new AdditiveHashSet()),
-
-                new Meal("Kawa", 5m, MealType.Beverage, new NullAdditiveFactory(), new NullAdditiveFactory(), new AdditiveHashSet()),
-                new Meal("Herbata", 5m, MealType.Beverage, new NullAdditiveFactory(), new NullAdditiveFactory(), new AdditiveHashSet()),
-                new Meal("Cola", 5m, MealType.Beverage, new NullAdditiveFactory(), new NullAdditiveFactory(), new AdditiveHashSet())
-            };
-
             Meals = CreateMeals().ToLookup<Meal, MealType>(r => r.Type);
         }
 
